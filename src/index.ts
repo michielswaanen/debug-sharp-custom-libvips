6d81e8351442ff0d51acac79fb14a0a97f029efc
Michiel Swaanen <michiel.swaanen@hotmail.com>
2024-06-07 11:07:04 +0200

    :sparkles: Added async/await

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,23 @@ import sharp from 'sharp';
 // Formats that we want to test
 const formats = ["heic", "jpeg", "pdf"]
 
-for (const format of formats) {
-    const source = `assets/example.${format}`;
-    const output = `output.${format}.jpg`;
+const runTests = async () => {
+    console.log("🚀 Testing sharp with direct file reference")
 
-    sharp(source)
-        .resize(300, 200)
-        .toFile(output, (err, info) => {
-            if (err) {
-                console.log(`❌ Error converting ${format}`)
-            } else {
-                console.log(`✅ Success converting ${format}`);
-            }
-        });
+    for (const format of formats) {
+        const source = `assets/example.${format}`;
+        const output = `output.${format}.jpg`;
+
+        await sharp(source)
+            .resize(300, 200)
+            .toFile(output, (err, info) => {
+                if (err) {
+                    console.log(`❌ Error converting ${format}`)
+                } else {
+                    console.log(`✅ Success converting ${format}`);
+                }
+            });
+    }
 }
+
+runTests();
